250583e92be675d7ac206c5e698290cf83035efd
StormedBubbles <80055191+StormedBubbles@users.noreply.github.com>
2022-11-08 00:44:31 -0500

    pong tweaks

--- src/osd/modules/input/input_retro.cpp
+++ src/osd/modules/input/input_retro.cpp
@@ -999,7 +999,23 @@ void retro_osd_interface::process_lightgun_state(running_machine &machine)
 	  //Correct ratio and offset for Pong
 	  {
 		 gun_x_scaled[j] = gun_x_raw[j] * 2;
-		 gun_y_scaled[j] = gun_y_raw[j] * 1.1 - 12732;
+
+		 if(gun_y_raw[j] * 1.1 - 12732 <= -31200)
+		 {
+		    gun_y_scaled[j] = gun_y_raw[j] * 1.1 - 14312;
+		 }
+		 else if(gun_y_raw[j] * 1.1 - 12732 > -31200 && gun_y_raw[j] * 1.1 - 12732 <= 5500)
+		 {
+			gun_y_scaled[j] = gun_y_raw[j] * 1.1 - 13522;
+		 }
+		 else if(gun_y_raw[j] * 1.1 - 12732 > 5500 && gun_y_raw[j] * 1.1 - 12732 <= 12000)
+		 {
+			gun_y_scaled[j] = gun_y_raw[j] * 1.1 - 13127;
+		 }
+		 else
+		 {
+			gun_y_scaled[j] = gun_y_raw[j] * 1.1 - 12732;
+		 }
 	  }
       else if (!core_stricmp(machine.system().name, "ppsatan") || !core_stricmp(machine.system().parent, "ppsatan"))
 	  //Correct ratio and offset for Poka Poka Satan
